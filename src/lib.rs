c8e4e33f614c8aae9601cd88746ecaf5b08578a8
tm-hack <takufrom1124@gmail.com>
2023-02-11 02:49:42 +0900

    プロジェクトをバイナリ（main）とロジック（lib）に分離した

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
