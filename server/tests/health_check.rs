b724fb1fe86e18f85f7daaf9791344cb7641b822
tm-hack <takufrom1124@gmail.com>
2023-02-13 18:35:28 +0900

    テスト終了時の明示的なdropを追加した

--- server/tests/health_check.rs
+++ server/tests/health_check.rs
@@ -4,7 +4,7 @@ use std::net::TcpListener;
 async fn health_check_works() {
     // Arrange
     let app = App::new();
-    let address = format!("{}:{}", app.address, app.port);
+    let address = format!("http:{}:{}", app.ip_addr, app.port);
     let client = reqwest::Client::new();
 
     // Act
@@ -23,18 +23,19 @@ async fn health_check_works() {
 }
 
 struct App {
-    address: String,
+    ip_addr: String,
     port: u16,
 }
 
 impl App {
     fn new() -> App {
-        let address = ("127.0.0.1:0").to_string();
-        let listner = TcpListener::bind(&address).expect("Failed to bind address");
+        let ip_addr = "127.0.0.1".to_string();
+        let bind_addr = format!("{ip_addr}:0");
+        let listner = TcpListener::bind(bind_addr).expect("Failed to bind address");
         let port = listner.local_addr().unwrap().port();
         let server = zero2prod::run(listner).expect("Failed to bind address");
         let _ = tokio::spawn(server);
 
-        App { address, port }
+        App { ip_addr, port }
     }
 }
